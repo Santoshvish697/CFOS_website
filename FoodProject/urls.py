c954e006464a254d849089ce75e414b696fa9b45
nisarga3 <ms.nisarga3@gmail.com>
2023-02-06 07:21:42 +0530

    solves merge conflict

--- FoodProject/urls.py
+++ FoodProject/urls.py
@@ -21,47 +21,44 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-        # ---- HOME ---- 
-        path('',views.foodapp), 
-        path('home',views.foodapp), 
-            
-        # ---- FOOD ---- 
-        path('addfoods',views.addfood),
-        path('deletefood/<int:FoodId>',views.deletefood),
-        path('getfood/<int:FoodId>',views.getfood),
-        path('editfood/<int:FoodId>',views.updatefood),
-        path('allfood',views.showfood),
-            
-        # ---- CUSTOMER ---- 
-        path('addcustomer',views.addcust),
-        path('deletecustomer/<int:CustId>',views.deletecust),
-        path('getcustomer',views.getcust),
-        path('editcustomer/<int:CustId>',views.updatecust),
-        path('allcustomer',views.showcust),
-    
-        # ---- CUSTOMER ---- 
-        path('updatepasswd',views.updatepasswd),
-        path('updatepassword',views.changepass),
-    
-        # ---- LOGIN ---- 
-<<<<<<< HEAD
-        # path('login',views.login),
-=======
-        path('login',views.login),
->>>>>>> 2db205a6 (hi)
-	path('dologin',views.doLogin),
-	path('logout',views.doLogout),
-	
-	# ---- CART ---- 
-	path('addtocart/<int:FoodId>',views.addcart),
-	path('allcart',views.showcart),
-	path('deletecart/<int:CartId>',views.delcart),
-	path('updateqnty/<str:s>',views.updateQNT),
-	
-	# ---- ORDER ---- 
-	path('placeorder',views.placeorder),
-	path('orders',views.getorder),
+
+    # ---- HOME ----
+    path('', views.foodapp),
+    path('home', views.foodapp),
+
+    # ---- FOOD ----
+    path('addfoods', views.addfood),
+    path('deletefood/<int:FoodId>', views.deletefood),
+    path('getfood/<int:FoodId>', views.getfood),
+    path('editfood/<int:FoodId>', views.updatefood),
+    path('allfood', views.showfood),
+
+    # ---- CUSTOMER ----
+    path('addcustomer', views.addcust),
+    path('deletecustomer/<int:CustId>', views.deletecust),
+    path('getcustomer', views.getcust),
+    path('editcustomer/<int:CustId>', views.updatecust),
+    path('allcustomer', views.showcust),
+
+    # ---- CUSTOMER ----
+    path('updatepasswd', views.updatepasswd),
+    path('updatepassword', views.changepass),
+
+    # ---- LOGIN ----
+    path('login', views.login),
+    path('dologin', views.doLogin),
+    path('logout', views.doLogout),
+
+    # ---- CART ----
+    path('addtocart/<int:FoodId>', views.addcart),
+    path('allcart', views.showcart),
+    path('deletecart/<int:CartId>', views.delcart),
+    path('updateqnty/<str:s>', views.updateQNT),
+
+    # ---- ORDER ----
+    path('placeorder', views.placeorder),
+    path('orders', views.getorder),
 ]
 if settings.DEBUG:
-	urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
